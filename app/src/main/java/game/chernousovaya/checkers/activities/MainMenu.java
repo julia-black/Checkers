cfbd15ca4a317f65780062eef1901a0cc03d3149
julia-black <b-r-cat@ya.ru>
2017-11-12 19:26:28 +0400

    Create MainMenu and GameActivity with board and checkers (but this need remade with array)

---
+++ app/src/main/java/game/chernousovaya/checkers/activities/MainMenu.java
@@ -0,0 +1,40 @@
+package game.chernousovaya.checkers.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import game.chernousovaya.checkers.R;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        final Button startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                startButton.setBackgroundResource(R.drawable.start_but_push);
+                Intent intent = new Intent(MainMenu.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        final Button startButton = (Button) findViewById(R.id.startButton);
+        startButton.setBackgroundResource(R.drawable.start_but_down);
+
+    }
+
+
+}
